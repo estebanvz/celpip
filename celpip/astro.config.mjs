b39b91a69393cf001ef688d7081ff2b144892d31
esteban vilca <evilcazu@gmail.com>
2025-02-14 06:09:34 +0000

    fix: change base

--- celpip/astro.config.mjs
+++ celpip/astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://estebanvz.github.io',
+  base: '/celpip/',
   vite: {
     plugins: [tailwindcss()],
     server: {
