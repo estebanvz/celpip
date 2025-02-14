aa8d5f65bcfa3ec16aaa792a45f3333cc94a5cf8
esteban vilca <evilcazu@gmail.com>
2025-02-14 06:06:04 +0000

    fix: change base

--- celpip/astro.config.mjs
+++ celpip/astro.config.mjs
@@ -5,14 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://estebanvz.github.io',
-  base: 'celpip',
-    vite: {
-      plugins: [tailwindcss()],
-      server:{
-        host: "0.0.0.0",
-        hmr: { clientPort: 4321 },
-        port: 4321, 
-        watch: { usePolling: true }
-      }
-    },
-  });
+  vite: {
+    plugins: [tailwindcss()],
+    server: {
+      host: "0.0.0.0",
+      hmr: { clientPort: 4321 },
+      port: 4321,
+      watch: { usePolling: true }
+    }
+  },
+});
